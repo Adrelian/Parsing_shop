7e8471765fada90467380210c0ae22f239f0a489
Anatolii <feelmysmell@yandex.ru>
2023-02-25 00:56:45 +0500

    Готовая функция парсинга по разделам каталога, глобальные разделы и подразделы парсятся по двух аргументам

--- Parsing.py
+++ Parsing.py
@@ -28,9 +28,7 @@ check_site(url_of_ETM, headers)
 response = requests.get(url_of_ETM, headers=headers)  # Получаем ответ от сайта
 soup = BeautifulSoup(response.content, "lxml")  # Варим суп и получаем весь сайт с каталогами
 
-data_link_catalog = soup.findAll("div", class_="CatalogCategories_wrapper__aOgzT")  # ищу все разделы сайта
-
-all_links = soup.findAll('a') # ищем все ссылки
+all_links = soup.findAll('a')  # ищем все ссылки
 list_link = []
 list_title = []
 # два списка с заголовками и ссылками
@@ -51,21 +49,58 @@ title_and_link = dict(zip(list_title, list_link))
 for value in title_and_link:
     title_and_link[value] = "https://www.etm.ru" + title_and_link[value]
 
-def  chapter_catalog():
+
+def del_unknown(number: int, list_with_something):
+    if number > 0:
+        list_with_something.pop()
+        number = number - 1
 
 
+def chapter_catalog(chapter_title_and_link: str, end_parsing: str):
+    """
+    Функция парсит по разделам сайта
+    :param end_parsing: Заголовок раздела, после которого останавливаем парсинг (последний раздел)
+    :param chapter_title_and_link: Из словаря нужно взять ссылку на раздел
+    :return:
+    """
+    # Создаём "суп" из начинки сайта ссылке
+    resp = requests.get(chapter_title_and_link, headers=headers)
+    soup_chapter = BeautifulSoup(resp.content, "lxml")
+
+    links = soup_chapter.findAll('a')  # ищем все ссылки на элементы
+    # пустые списки для ссылок и заголовков разделов
+    title_chapter = []
+    link_chapter = []
+    # перебираем все ссылки и заголовки, складываем в списки созданные ранее
+    for url in links:
+        link_ch = url.get("href")
+        link_chapter.append(link_ch)
+        title_ch = url.getText("CCatalogCategories_cardSubCategory__urEmp")
+        title_chapter.append(title_ch)
+        if title_ch == end_parsing:
+            break
+    # Удаляем не нужны ссылки
+    link_chapter.pop(0)
+    link_chapter.pop(0)
+    link_chapter.pop(0)
+    title_chapter.pop(0)
+    title_chapter.pop(0)
+    title_chapter.pop(0)
+    data_dictionary = dict(zip(title_chapter, link_chapter))
+
+    for value in data_dictionary:
+        data_dictionary[value] = "https://www.etm.ru/catalog/" + data_dictionary[value]
+
+    return data_dictionary
 
 
+def get_name_and_price():
+    pass
 
 
+# Раздел с проводами
+cable = chapter_catalog(title_and_link["Кабели, провода и изделия для прокладки кабеля"], "Промышленные электрические соединители")
 
-
-
-
-
-# data_schity_title = soup.findAll("div", class_ = "CatalogCategories_card__Y8sOG") # заголовки разделов
-# print(data_schity_title)
-
-# data = soup.find("div", class_="jss173")
-# print(data)
-# # name = data.find("div", class_="jss195").text.replace("\n", "")
+# Раздел со светотехникой
+light = chapter_catalog(title_and_link["Светотехнические изделия"], "Аксессуары Системы Управления Освещеним (адаптеры, выключатели т.д.)")
+print(light)
