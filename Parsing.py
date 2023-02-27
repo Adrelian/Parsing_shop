55b13911fdd942158c243775c9f2640699df28c3
Anatolii <feelmysmell@yandex.ru>
2023-02-27 06:52:55 +0500

    Нужно проработать хранение информации обо всех каталогах

--- Parsing.py
+++ Parsing.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
-import pandas as pd
 
+# headers взял из своего браузера в запросах, нужен что бы "обмануть" сайт
 headers = {
     "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/108.0.0.0 YaBrowser/23.1.2.987 Yowser/2.5 Safari/537.36",
     "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9"
@@ -23,54 +23,62 @@ def check_site(site: str, head):
         print(f'{url}. Требуется проверка')
 
 
-check_site(url_of_ETM, headers)
+def create_catalog_all_production(site: str):
+    """
+    Функция создаёт основой каталог сайта в виде словаря из ключей в виде названия раздела и значений в виде ссылок на страницы подкаталогов
+    :param site: Основной сайт магазина
+    :return: Возвращает словарь из названия разделов и ссылок
+    """
+    response = requests.get(site, headers=headers)  # Получаем ответ от сайта
+    soup = BeautifulSoup(response.content, "lxml")  # Варим суп и получаем весь сайт с каталогами
 
-response = requests.get(url_of_ETM, headers=headers)  # Получаем ответ от сайта
-soup = BeautifulSoup(response.content, "lxml")  # Варим суп и получаем весь сайт с каталогами
-
-all_links = soup.findAll('a')  # ищем все ссылки
-list_link = []
-list_title = []
-# два списка с заголовками и ссылками
-for link in all_links:
-    link_url = link.get("href")
-    list_link.append(link_url)
-    title_url = link.getText("CatalogCategories_title__ahzrn")
-    list_title.append(title_url)
-    if title_url == "Программное обеспечение":
-        break
-# Удаляем лишние ссылки
-list_title.pop(0)
-list_title.pop(0)
-list_link.pop(0)
-list_link.pop(0)
-# Создаём словарь из заголовков и ссылок
-title_and_link = dict(zip(list_title, list_link))
-for value in title_and_link:
-    title_and_link[value] = "https://www.etm.ru" + title_and_link[value]
+    all_links = soup.findAll('a')  # ищем все ссылки
+    list_link = []  # cписок для ссылок
+    list_title = []  # список для названия разделов каталога
+    # складываем ссылки разделов и названия разделов в списки
+    for link in all_links:
+        link_url = link.get("href")
+        list_link.append(link_url)
+        title_url = link.getText("CatalogCategories_title__ahzrn")
+        list_title.append(title_url)
+        if title_url == "Программное обеспечение":
+            break
+    # Удаляем лишние ссылки
+    list_title.pop(0)
+    list_title.pop(0)
+    list_link.pop(0)
+    list_link.pop(0)
+    # Создаём словарь из заголовков и ссылок
+    title_and_link = dict(zip(list_title, list_link))
+    for value in title_and_link:
+        title_and_link[value] = "https://www.etm.ru" + title_and_link[value]
+    return title_and_link
 
 
-def del_unknown(number: int, list_with_something):
+def del_unknown(number: int, list_with_something):  # ПРОВЕРИТЬ РАБОТУ
     if number > 0:
         list_with_something.pop()
         number = number - 1
 
 
-def chapter_catalog(chapter_title_and_link: str, end_parsing: str):
+def chapter_catalog(main_catalog: dict, end_parsing: str, index: int):
     """
-    Функция парсит по разделам сайта
+    Функция парсит по разделам каталога, которые уже созданы выше, для получения названия и ссылок подкаталога
+    :param index: Индекс словаря
     :param end_parsing: Заголовок раздела, после которого останавливаем парсинг (последний раздел)
-    :param chapter_title_and_link: Из словаря нужно взять ссылку на раздел
-    :return:
+    :param main_catalog: Основной каталог товаров
+    :return: возвращает словарь из названия разделов и их ссылок
     """
-    # Создаём "суп" из начинки сайта ссылке
-    resp = requests.get(chapter_title_and_link, headers=headers)
-    soup_chapter = BeautifulSoup(resp.content, "lxml")
-
-    links = soup_chapter.findAll('a')  # ищем все ссылки на элементы
+    index = 0  # для движения по словарю
+    for chapter in main_catalog:
+        resp = requests.get(main_catalog[index], headers=headers) # Запрос на сайт
+        soup_chapter = BeautifulSoup(resp.content, "lxml") # Получение информации со всего сайта по запросу выше
+        links = soup_chapter.findAll('a')  # ищем все ссылки на элементы
+        main_catalog.setdefault(__key=[], []).
     # пустые списки для ссылок и заголовков разделов
     title_chapter = []
     link_chapter = []
+
     # перебираем все ссылки и заголовки, складываем в списки созданные ранее
     for url in links:
         link_ch = url.get("href")
@@ -79,6 +87,7 @@ def chapter_catalog(chapter_title_and_link: str, end_parsing: str):
         title_chapter.append(title_ch)
         if title_ch == end_parsing:
             break
+
     # Удаляем не нужны ссылки
     link_chapter.pop(0)
     link_chapter.pop(0)
@@ -86,25 +95,30 @@ def chapter_catalog(chapter_title_and_link: str, end_parsing: str):
     title_chapter.pop(0)
     title_chapter.pop(0)
     title_chapter.pop(0)
-    data_dictionary = dict(zip(title_chapter, link_chapter))
 
+    # Создаём словарь из названия разделов и ссылок
+    data_dictionary = dict(zip(title_chapter, link_chapter))
     for value in data_dictionary:
         data_dictionary[value] = "https://www.etm.ru/catalog/" + data_dictionary[value]
-
     return data_dictionary
 
 
-def get_name_and_price(chapter: str, tag: str):
+def get_name_and_price(chapter: str, tag: str):  # ФУНКЦИЯ НЕ ГОТОВАЯ. ТРЕБУЕТСЯ ПРОВЕРКА
     resp = requests.get(chapter, headers=headers)
     soup_chapter = BeautifulSoup(resp.content, "lxml")
     print(soup_chapter)
 
 
-# Раздел с проводами
-cable = chapter_catalog(title_and_link["Кабели, провода и изделия для прокладки кабеля"], "Промышленные электрические соединители")
-f = list(cable.values())[0]
+# Проверяем сайт на отклик состояния. Если 200 всё ОК, если другое число, то читаем документацию в этому числу
+check_site(url_of_ETM, headers)
 
-get_name_and_price(f, "js397")
-# Раздел со светотехникой
-light = chapter_catalog(title_and_link["Светотехнические изделия"], "Аксессуары Системы Управления Освещеним (адаптеры, выключатели т.д.)")
+# Получаем общий каталог сайта
+catalog_etm = create_catalog_all_production("https://www.etm.ru/catalog")
 
+# # Раздел с проводами
+# cable = chapter_catalog(title_and_link["Кабели, провода и изделия для прокладки кабеля"], "Промышленные электрические соединители")
+# f = list(cable.values())[0]
+#
+# get_name_and_price(f, "js397")
+# # Раздел со светотехникой
+# light = chapter_catalog(title_and_link["Светотехнические изделия"], "Аксессуары Системы Управления Освещеним (адаптеры, выключатели т.д.)")
