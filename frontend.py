b90d2e5de605dd6f8c137a20a0fc831d7043117b
Anatolii <feelmysmell@yandex.ru>
2023-05-21 00:28:57 +0500

    Тетсирование интерфейса

--- frontend.py
+++ frontend.py
@@ -10,28 +10,29 @@ class Window(QMainWindow):
         self.setWindowTitle("Моя программа")
         self.setGeometry(400, 400, 600, 600)  # Первые два числа это смещение по X и Y, вторые это размеры окна
 
+        self.new_text = QtWidgets.QLabel(self)
+
         self.main_text = QtWidgets.QLabel(self)
         self.main_text.setText("Это базовый текст")
         self.main_text.move(100, 100)
         self.main_text.adjustSize()
 
-        btn = QtWidgets.QPushButton(self)
-        btn.move(70, 150)
-        btn.setText("Кнопка")
-        btn.setFixedWidth(200)
-        btn.clicked.connect(add_label)
+        self.btn = QtWidgets.QPushButton(self)
+        self.btn.move(70, 150)
+        self.btn.setText("Кнопка")
+        self.btn.setFixedWidth(200)
+        self.btn.clicked.connect(self.add_label)
 
-
-def add_label():
-    print("add")
+    def add_label(self):
+        self.new_text.setText("Вторая надпись")
+        self.new_text.move(100, 50)
+        self.new_text.adjustSize()
 
 
 def application():
     app = QApplication(sys.argv)  # Запуск программы на компьютере и передаем те настройки, которые связаны именно с
     # этим компьютером
-    window = QMainWindow()  # Основное окно программы
-
-
+    window = Window()  # Основное окно программы
 
     window.show()  # открытие программы
     sys.exit(app.exec_())  # Закрытие программы
